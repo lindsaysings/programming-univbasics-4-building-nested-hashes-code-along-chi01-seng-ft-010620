6ce0f41ea3f58e56064738bb8720163a235ae180
Lindsay M <lindsaymecher@gmail.com>
2019-12-20 20:48:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,9 +11,8 @@ def second_challenge
         
       }
     ,
-    :hero_friends => {
-      
-    }
+    :hero_friends => [
+      ]
       }, 
     :capulet => {
       #your key/value pairs here!
