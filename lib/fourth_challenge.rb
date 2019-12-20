acaa363751881112138cf08df4e8952b86d3bc6b
Lindsay M <lindsaymecher@gmail.com>
2019-12-20 21:03:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [ "Benvolio" => {
         :name => "Benvolio",
-        :age => 
+        :age => ""
       }
         #your hashes here!
       ]
