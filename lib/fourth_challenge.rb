502590c50a0864ba0138e075e38d5c8c6c10ef98
Lindsay M <lindsaymecher@gmail.com>
2019-12-20 21:03:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,8 +12,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{
-        :name
+      :heroine_friends => [ "Benvolio" => {
+        :name => "Benvolio",
+        :age => 
       }
         #your hashes here!
       ]
