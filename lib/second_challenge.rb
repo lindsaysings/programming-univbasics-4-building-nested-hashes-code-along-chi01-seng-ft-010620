dade1c9cb8bcffbf79e4994468c85a672fe7a1b9
Lindsay M <lindsaymecher@gmail.com>
2019-12-20 20:53:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,22 +2,21 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-      
+        
       },
+      
       :matriarch => {
         
       },
+      
       :hero => {
         
-      }
-    ,
+      },
+    
     :hero_friends => [
       ]
       }, 
-    :capulet => {
-      
-      },
-      
+    
     :capulet => {
       :patriarch => {
         
